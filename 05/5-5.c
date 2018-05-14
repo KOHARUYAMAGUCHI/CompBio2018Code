5b908e948834b8bf25447d53f04f5b3fe0edb0f1
KOHARUYAMAGUCHI <otorisama10330@gmail.com>
2018-05-14 14:57:55 +0900

    update

--- 05/5-5.c
+++ 05/5-5.c
@@ -3,12 +3,16 @@
 #include<time.h>
 
 int main(void){
-  int r,M,N,x,i;
+  int r,M,N,x,i,a;
   srand(time(NULL));
   M=1;
   N=6;
   x=0;
+  a=0;
 
+for(t=1;t<1000;t++){
+
+}
   for(i=0;i<100;i++){
     r=rand()%(N-M+1)+M;
     x=x+r;
